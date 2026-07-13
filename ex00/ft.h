2804c99549007cab0fd90c2d34638e62a7a7df6a
Shuin Kang Wu <shuwu@c2r5s2.42singapore.sg>
2026-07-13 15:03:48 +0800

    still working on ft.h. got a Error: PREPROC_BAD_INDENT   (line:  15, col:   1):	Bad preprocessor indentation error

--- ex00/ft.h
+++ ex00/ft.h
@@ -6,8 +6,17 @@
 /*   By: shuwu <shuwu@student.42singapore.sg>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/13 14:36:34 by shuwu             #+#    #+#             */
-/*   Updated: 2026/07/13 14:37:02 by shuwu            ###   ########.fr       */
+/*   Updated: 2026/07/13 15:01:22 by shuwu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_H
+#define FT_H
 
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
