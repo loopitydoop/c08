81b18d8af80dd4cefe77be497a78989dab977320
Shuin Kang Wu <shuwu@c2r3s6.42singapore.sg>
2026-07-15 17:20:54 +0800

    fixed ft_point.h, forgot to add #ifndef guards

--- ex03/ft_point.h
+++ ex03/ft_point.h
@@ -6,16 +6,20 @@
 /*   By: shuwu <shuwu@42singapore.sg>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/14 13:19:30 by shuwu             #+#    #+#             */
-/*   Updated: 2026/07/15 11:04:31 by shuwu            ###   ########.fr       */
+/*   Updated: 2026/07/15 17:19:15 by shuwu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // need to create a typedef struct for t_point
 // normal struct won't work since the declaration only uses the t_point
 // alias directly when declaring a new t_point datatype var.
+#ifndef FT_POINT_H
+# define FT_POINT_H
 
 typedef struct s_point
 {
 	int	x;
 	int	y;
 }	t_point;
+
+#endif
