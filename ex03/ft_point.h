59b2423ad12524d9725bfa5d02f0356a66335ba0
Shuin Kang Wu <shuwu@c2r5s12.42singapore.sg>
2026-07-16 09:04:23 +0800

    just doing final checks on the guard statements

--- ex03/ft_point.h
+++ ex03/ft_point.h
@@ -6,13 +6,14 @@
 /*   By: shuwu <shuwu@42singapore.sg>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/14 13:19:30 by shuwu             #+#    #+#             */
-/*   Updated: 2026/07/15 17:19:15 by shuwu            ###   ########.fr       */
+/*   Updated: 2026/07/16 09:00:01 by shuwu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // need to create a typedef struct for t_point
 // normal struct won't work since the declaration only uses the t_point
 // alias directly when declaring a new t_point datatype var.
+// ok friends, guard statements are VERY important ok.
 #ifndef FT_POINT_H
 # define FT_POINT_H
 
